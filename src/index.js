56ab99223bc1bc33a64961974ce0748602fa82bb
Siarhei Mitasau <darkenRahl1893@gmail.com>
2019-03-23 23:18:14 +0300

    problem solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let originalNumber = number;
+  let countBase = base;
+
+  for (let i = 2; i <= base; i++) {
+
+    if (countBase % i === 0) {
+      let baseCount = 0;
+
+      while (countBase % i === 0) {
+        baseCount++;
+        countBase = parseInt(countBase / i);
+      }
+
+      let currSum = 0;
+      let currNumber = number;
+
+      while (currNumber / i > 0) {
+        currNumber = parseInt(currNumber / i);
+        currSum += currNumber;
+      }
+
+      originalNumber = Math.min(originalNumber, currSum / baseCount);
+    }
+
+  }
+  return parseInt(originalNumber);
 }
